194147699862e5bfda383f7ab29b113c47b82df4
altaparro <matiasaltaparro@hotmail.com>
2024-06-11 13:36:09 -0300

    fix carrito

--- src/controllers/carts.controllers.js
+++ src/controllers/carts.controllers.js
@@ -45,4 +45,77 @@ async function crearCarrito(req, res) {
     }
   }
 
-  module.exports = { crearCarrito }
+  async function obtenerCarritoPorId(req, res) {
+    try {
+      const filePath = path.join(__dirname, '..', '..', 'carts.json');
+      const data = await fs.readFile(filePath, 'utf-8');
+      const carts = JSON.parse(data);
+      // Obtener el ID del carrito desde los parámetros de la ruta
+      const cartId = req.params.id;
+  
+      // Buscar el carrito con el ID especificado
+      const cart = carts.find(c => c.id === parseInt(cartId));
+  
+      if (cart) {
+        res.status(200).json({
+          ok: true,
+          status: 200,
+          body: cart,
+        });
+      } else {
+        res.status(404).json({
+          ok: false,
+          status: 404,
+          error: `Carrito con ID ${cartId} no encontrado`,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        ok: false,
+        status: 500,
+        error: error.message,
+      });
+    }
+  }
+
+  async function agregarProducto(req, res) {
+    try {
+      const filePath = path.join(__dirname, '..', '..', 'carts.json');
+      const data = await fs.readFile(filePath, 'utf-8');
+      const carts = JSON.parse(data);
+  
+      // Buscar el carrito con el ID especificado
+      const cartId = req.params.cid;
+      const cart = carts.find(c => c.id === parseInt(cartId));
+      const quantity = parseInt(req.body.quantity)
+      //buscar producto
+      const productId = req.params.pid
+      const existeProducto = cart.products.find(p => p.product === productId)
+      
+      if (existeProducto) {
+        existeProducto.quantity += quantity
+      } else {
+        cart.products.push({ product: productId, quantity})
+      }
+
+      // Escribir el array actualizado de carritos de vuelta al archivo
+      await fs.writeFile(filePath, JSON.stringify(carts, null, 2));
+  
+      // Devolver una respuesta con el nuevo carrito creado
+      res.status(201).json({
+        ok: true,
+        status: 201
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        ok: false,
+        status: 500,
+        error: error.message,
+      });
+    }
+  }
+  
+
+  module.exports = { crearCarrito, obtenerCarritoPorId, agregarProducto }
--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -3,4 +3,8 @@ const cartsController = require("../controllers/carts.controllers")
 
 cartsRouter.post("/carts/crearCarrito", cartsController.crearCarrito)
 
+cartsRouter.get("/carts/obtenerCarritoPorId/:id", cartsController.obtenerCarritoPorId)
+
+cartsRouter.post("/carts/agregarProducto/:cid/product/:pid", cartsController.agregarProducto)
+
 module.exports = cartsRouter
