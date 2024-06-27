9a61f42000c4ea3baa1f295b7c95ed1f3bbb1828
altaparro <matiasaltaparro@hotmail.com>
2024-06-27 10:18:42 -0300

    config websockets

--- src/app.js
+++ src/app.js
@@ -2,10 +2,16 @@ const express = require("express");
 const exphbs = require("express-handlebars")
 const app = express()
 const PUERTO = 8081
+const PUERTO2 = 8082
 const productoRouter = require("../src/routes/products.routes");
 const cartsRouter = require("../src/routes/carts.routes")
 const viewsRouter = require("../src/routes/views.router")
 const socket = require("socket.io")
+const { WebSocketServer } = require('ws');
+const wss = new WebSocketServer({ noServer: true });
+
+const path = require('path');
+const products = path.join(__dirname, '..', '..', 'productos.json');
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -22,4 +28,41 @@ const httpServer = app.listen(PUERTO, () => {
     console.log('Servidor ON');
 })
 
-const io = new socket.Server(httpServer)
+// Manejar conexiones WebSocket
+wss.on('connection', (ws) => {
+    ws.send(JSON.stringify(products)); // Enviar lista inicial de productos
+  
+    ws.on('message', (message) => {
+      const { action, product } = JSON.parse(message);
+      if (action === 'add') {
+        products.push(product);
+      } else if (action === 'delete') {
+        const index = products.findIndex(p => p.id === product.id);
+        if (index !== -1) {
+          products.splice(index, 1);
+        }
+      }
+      // Notificar a todos los clientes
+      wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(products));
+        }
+      });
+    });
+  });
+  
+  // Integrar WebSocket con el servidor HTTP de Express
+  const server = app.listen(PUERTO2, () => {
+    console.log(`Servidor iniciado en http://localhost:${PUERTO2}`);
+  });
+  
+  server.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+      wss.emit('connection', ws, request);
+    });
+  });
+
+// Endpoint para renderizar la vista realTimeProducts.handlebars
+app.get('/realtimeproducts', (req, res) => {
+    res.render('realTimeProducts');
+  });
